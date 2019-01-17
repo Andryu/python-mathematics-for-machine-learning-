7e1f9a92b982e356e35c65afc8a92c8a7ff53a51
Shunsuke Andoh <shunsuke.andoh@gmail.com>
2019-01-18 06:05:46 +0900

    add 5-2-6

--- chapter5/5-2-1.py
+++ chapter5/5-2-1.py
@@ -1,4 +1,4 @@
-#%% Section 5-2-1
+#%% サンプル 5-2-1
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,7 +12,7 @@ train_y = train[:,1]
 plt.plot(train_x, train_y,'o')
 plt.show()
 
-#%% Section 5-2-2
+#%% サンプル 5-2-2
 
 # パラメータ初期化
 theta0 = np.random.rand()
@@ -46,4 +46,43 @@ train_z = standardize(train_x)
 print('train_z: %s' % train_z)
 
 plt.plot(train_z, train_y, 'o')
+plt.show()
+
+
+#%% サンプル 5-2-5
+
+# 学習率
+ETA = 1e-3
+
+# 誤差の差分
+diff = 1
+
+# 更新回数
+count = 0
+
+# 学習を繰り返す
+
+error = E(train_z, train_y)
+while diff > 1e-2:
+    tmp0 = theta0 - ETA * np.sum((f(train_z) - train_y))
+    tmp1 = theta1 - ETA * np.sum((f(train_z) - train_y) * train_z)
+
+    # Parameter更新
+    theta0 = tmp0
+    theta1 = tmp1 
+
+    # 全開の誤差との差分計算
+    current_error = E(train_z, train_y)
+    diff = error - current_error
+    error = current_error
+
+    # ログの出力
+    count += 1
+    log = '{}回目: theta0 = {: .3f}, theta1 = {:.3f}, 差分 = {:.4f}'
+    print(log.format(count, theta0, theta1, diff))
+
+#%% 5-2-6
+x = np.linspace(-3, 3, 100)
+plt.plot(train_z, train_y, 'o')
+plt.plot(x, f(x))
 plt.show()
